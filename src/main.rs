8dd21168909c7ca1e31a0ff368c70c405b9cd4b4
trev <trev@trevrosa.dev>
2025-08-02 23:16:28 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::process::Command;
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 2 {
+        return;
+    }
+
+    let jar = &args[1];
+
+    Command::new("java")
+        .args(&["-jar", jar])
+        .status()
+        .expect("could not run jar");
+}
